89be8ebeae436ea4caa034599cc8233bd829a0b4
Pojzo <pojzinko8@gmail.com>
2021-12-06 23:49:20 +0100

    Added some constants

--- src/config.h
+++ src/config.h
@@ -1,2 +1,9 @@
+static const unsigned int SCREEN_WIDTH = 640;
+static const unsigned int SCREEN_HEIGHT = 640;
 static const unsigned int MAX_GRID_WIDTH = 100;
 static const unsigned int MAX_GRID_HEIGHT = 100;
+static const unsigned char BACKGROUND_R = 255;
+static const unsigned char BACKGROUND_G = 255;
+static const unsigned char BACKGROUND_B = 255;
+static const unsigned char BACKGROUND_A = 255;
+static const char *WINDOW_NAME = "Langtons ant";
